7da434be7444633d26414cee6a51c441fc31a2a3
Juan H <jhuertas1101@gmail.com>
2019-09-18 19:08:33 +0200

    all good

--- fandora/src/app/app.module.ts
+++ fandora/src/app/app.module.ts
@@ -4,21 +4,15 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { HomeComponent } from './home/home.component';
-=======
 import {MatCardModule} from '@angular/material';
 import { CardsComponent } from './components/cards/cards.component';
->>>>>>> 4373302dc78fe7352a8aacfed26ba21ee605db23
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     HomeComponent,
-=======
     CardsComponent
->>>>>>> 4373302dc78fe7352a8aacfed26ba21ee605db23
   ],
   imports: [
     BrowserModule,
