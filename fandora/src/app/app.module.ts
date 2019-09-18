006934b7cbae2133f5bace6f2c0c598e9a12e0f1
Juan H <jhuertas1101@gmail.com>
2019-09-18 18:15:28 +0200

    cards

--- fandora/src/app/app.module.ts
+++ fandora/src/app/app.module.ts
@@ -5,10 +5,12 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardsComponent } from './components/cards/cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
